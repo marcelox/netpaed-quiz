7828c9b553fe0a11875d2e0b0a29860fd9209f1c
marcelo cordeiro <mc@points.de>
2024-06-02 16:49:43 +0200

    Add prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  import Escape from './Escape.svelte';
-  import NetLogo from './assets/netpaed_jena_logoTrans.png';
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Escape from "./Escape.svelte";
+  import NetLogo from "./assets/netpaed_jena_logoTrans.png";
 </script>
 
 <main class="grid grid-rows-[100px_1fr_100px] h-full">
-    <img alt='' class="mx-auto aspect-auto max-w-28" src={NetLogo}/>
-    <Escape />
+  <img alt="" class="mx-auto aspect-auto max-w-28" src={NetLogo} />
+  <Escape />
 </main>
 
 <style></style>
